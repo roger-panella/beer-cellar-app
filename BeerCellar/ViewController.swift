96e001172accde49e2c0faa802034220687e1f7d
Ben Munge <ben.munge@orbitz.com>
2017-03-08 21:26:38 -0600

    initial app creation

---
+++ BeerCellar/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  BeerCellar
+//
+//  Created by Ben Munge on 3/8/17.
+//  Copyright © 2017 beer-cellar-app. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .orange
+    }
+}
